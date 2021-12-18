fa0a541d716b340fe6a3facd18b271480e49fa36
Leah Atkins <leah.atkins84@gmail.com>
2021-12-17 23:29:10 -0600

    completed questions 3a-d plus stretch

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -35,6 +35,9 @@ for (i=2; i <11; i++){
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
+for (let i=5; i> -1; i--){
+  console.log(i);
+}
 
 
 // 2. For of loops
@@ -52,9 +55,20 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+ i=0
+while (i< stars.length){
+  console.log(stars[i]);i++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+i=0
+while(i < 5){
+  console.log(i);i++;
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+i=10
+while(i >4){
+  console.log(i);i--;
+}
