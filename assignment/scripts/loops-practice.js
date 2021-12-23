1d177b2aedf6025809b89f0e8b8ef9b6c25f4f00
Leah Atkins <leah.atkins84@gmail.com>
2021-12-22 21:27:00 -0600

    corrections made

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -27,7 +27,7 @@ for (let i=3; i<6; i++){
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-for (i=2; i <11; i++){
+for (let i=2; i <11; i++){
   if (i % 2 === 0) {
     console.log(i);
   }
@@ -55,14 +55,14 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
- i=0
+let i=0
 while (i< stars.length){
   console.log(stars[i]);i++;
 }
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-i=0
-while(i < 5){
+ i=0
+while(i < 6){
   console.log(i);i++;
 }
 
